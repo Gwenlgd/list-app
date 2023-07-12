31e74763546cbf66628dad5e93fb6f2c087002db
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2023-07-12 19:24:47 +0200

    Init: Validations for models Items & Lists

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  # has_many :list a checker ?
+
+  validates :name, presence: true
+  validates :category, acceptance: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
 end
