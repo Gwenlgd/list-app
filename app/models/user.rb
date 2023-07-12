4c681fe9aa0fa6553686b917ac350a32a3139d61
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2023-07-12 19:09:19 +0200

    Init: Models for Lists, Items & Groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :lists, dependent: :destroy
+  has_many :groups
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  # validates :age, presence: true
+  # validates :gender, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  # validates :password_confirmation
 end
