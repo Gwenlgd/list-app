da5e4cac1b2594fd7a69eaf9bc8ccb8e1f1669a8
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2023-08-02 15:31:37 +0200

    Init: show pages

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,31 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
 
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+  end
+
+
+
+private
+
+# manque ID ITEMS?
+  def list_params
+    params.require(:list).permit(:list_name, :description)
+  end
+
+  def set_list
+    @listing = Listing.find(params[:id])
+  end
 end
