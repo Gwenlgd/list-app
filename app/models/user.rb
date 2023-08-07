53839e5061d65ec2155924ede8552a775f739f91
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2023-08-07 14:41:56 +0200

    Init: Lists, adding references, init index, show, edit, destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :lists, dependent: :destroy
+
   # has_many :lists, dependent: :destroy
   # has_many :groups
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 
 puts "Cleaning database..."
 User.destroy_all
 
 puts "Creating users..."
 
-gwen = User.create!(surname: "Gwenlgd", first_name: "Gwen", last_name: "Leg", address: "Paris", email: "gwenlgd@paradise.com", password: "paradise")
+gwen = User.create!(username: "Gwenlgd", first_name: "Gwen", last_name: "Leg", address: "Paris", email: "gwenlgd@paradise.com", password: "paradise")
+mathilde = User.create!(username: "Mathildelgd", first_name: "Math", last_name: "Leg", address: "Char", email: "mathildelgd@paradise.com", password: "paradise")
+
+puts "Creating lists..."
+
+listone = List.create!(name_list: "My Birthday", description: "Ideas for my birthday", user: gwen)
+listtwo = List.create!(name_list: "Christmas 2023", description: "Christmas's Ideas", user: mathilde)
+listthree = List.create!(name_list: "Alix's Birthday", description: "Ideas for Alix", user: mathilde)
